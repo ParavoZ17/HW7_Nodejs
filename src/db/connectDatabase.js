87019a855ab9c48abfddaaaeba4e146c5a3f8fb1
Mykola Naumenko <1712nn2012@gmail.com>
2025-10-22 09:43:30 +0200

    add model APP and make migration

--- src/db/connectDatabase.js
+++ src/db/connectDatabase.js
@@ -1,10 +1,12 @@
 import sequelize from "./sequelize.js";
+import App from "./models/App.js";
 
 
 const connectDatabase = async()=> {
     try {
         await sequelize.authenticate();
         console.log("Successfully connect database");
+        await App.sync({ alter: true });
     
     }
     catch(error) {
